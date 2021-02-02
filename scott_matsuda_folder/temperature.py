923fc1c4fc84d5b956af9595daa649f3918d2172
Scott Matsuda <smatsuda2014@gmail.com>
2021-02-01 17:10:44 -0800

    adding temperature.py challenge

---
+++ scott_matsuda_folder/temperature.py
@@ -0,0 +1,23 @@
+# 1.
+celsius_100 = (100 -32) * 5/9
+print('celsius_100: ', celsius_100)
+
+# The resulting temperature is a float 37.77777777778. 
+# It's a float because there is a decimal point.
+
+# 2.
+celsius_0 = (0 - 32) * 5/9
+print('celsius_0: ', celsius_0)
+
+# celsius_0 = -17.7777777777778
+
+# 3.
+print((34.2 - 32) * 5/9)
+
+# 4.
+print('celsius_5: ', (5 * 9/5) + 32)
+
+# 5. 
+print('celsius_30.2: ', (30.2 * 9/5) + 32)
+# 30.2 degrees celsius (86.36 degrees F) is hotter than 85.1 degrees F
+
